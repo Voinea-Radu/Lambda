c50697d9ac74e0f464e163b044e9bb2fef4ce921
Radu Voinea <radu.voinea.mihai@gmail.com>
2022-12-22 14:25:54 +0200

    Added reflection

---
+++ src/main/java/dev/lightdream/lambda/reflection/Reflector.java
@@ -0,0 +1,50 @@
+package dev.lightdream.lambda.reflection;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Reflector {
+
+    private final List<Class<?>> classes;
+
+    public Reflector(String packageName) {
+        this.classes = getClasses(packageName);
+    }
+
+    public List<Class<?>> getClassesAnnotatedWith(Class<? extends Annotation> annotation) {
+        return classes.stream().filter(clazz -> clazz.isAnnotationPresent(annotation)).collect(Collectors.toList());
+    }
+
+    private List<Class<?>> getClasses(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+
+        if (stream == null) {
+            return new ArrayList<>();
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toList());
+    }
+
+    private Class<?> getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "."
+                    + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            // handle the exception
+        }
+        return null;
+    }
+
+
+}
