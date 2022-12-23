eef1ac4d79c7268f87d4afb346532ebae0c1efd4
Radu Voinea <radu.voinea.mihai@gmail.com>
2022-12-23 10:50:46 +0200

    Added Reflector#getMethodsAnnotatedWith

--- src/main/java/dev/lightdream/lambda/reflection/Reflector.java
+++ src/main/java/dev/lightdream/lambda/reflection/Reflector.java
@@ -2,20 +2,25 @@ package dev.lightdream.lambda.reflection;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class Reflector {
 
-    private final ReflectionMain main;
     private final List<Class<?>> classes;
 
     public Reflector(ReflectionMain main, String packageName) {
-        this.main = main;
-        this.classes = getClasses(packageName);
+        this.classes = getClasses(main, packageName);
+    }
+
+    @SuppressWarnings("ArraysAsListWithZeroOrOneArgument")
+    public Reflector(Class<?> clazz) {
+        this.classes = Arrays.asList(clazz);
     }
 
     public List<Class<?>> getClassesAnnotatedWith(Class<? extends Annotation> annotation) {
@@ -23,6 +28,20 @@ public class Reflector {
                 .stream()
                 .filter(aClass -> aClass.isAnnotationPresent(annotation))
                 .collect(Collectors.toList());
+    }
+
+    public List<Method> getMethodsAnnotatedWith(Class<? extends Annotation> annotation) {
+        List<Method> methods = new ArrayList<>();
+
+        classes.forEach(aClass ->
+                methods.addAll(Arrays
+                        .stream(aClass.getDeclaredMethods())
+                        .filter(method -> method.isAnnotationPresent(annotation))
+                        .collect(Collectors.toList())
+                )
+        );
+
+        return methods;
     }
 
     @SuppressWarnings("unchecked")
@@ -34,7 +53,7 @@ public class Reflector {
                 .collect(Collectors.toList());
     }
 
-    private List<Class<?>> getClasses(String packageName) {
+    private List<Class<?>> getClasses(ReflectionMain main, String packageName) {
         List<Class<?>> classes = new ArrayList<>();
 
         URL resource = ClassLoader.getSystemClassLoader().getResource(packageName.replaceAll("[.]", "/"));
@@ -49,10 +68,10 @@ public class Reflector {
                 for (File file : files) {
                     if (file.isDirectory()) {
                         // Recursively search for classes in subpackages
-                        classes.addAll(getClasses(packageName + "." + file.getName()));
+                        classes.addAll(getClasses(main, packageName + "." + file.getName()));
                     } else if (file.getName().endsWith(".class")) {
                         // Add the class to the list
-                        classes.add(getClass(file.getName(), packageName));
+                        classes.add(getClass(main, file.getName(), packageName));
                     }
                 }
             }
@@ -62,7 +81,7 @@ public class Reflector {
     }
 
 
-    private Class<?> getClass(String className, String packageName) {
+    private Class<?> getClass(ReflectionMain main, String className, String packageName) {
         String fullClassName = packageName + "." + className.substring(0, className.lastIndexOf('.'));
         return main.getClass(fullClassName);
     }
